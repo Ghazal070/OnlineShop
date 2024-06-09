fd8b2415980041471db4818ebb8f9a83e68fcc25
ghazal <ghazaltorabi070@gmail.com>
2024-06-09 23:43:18 +0330

    Connection to SQL DB

---
+++ src/main/java/util/ApplicationProperties.java
@@ -0,0 +1,7 @@
+package util;
+
+public class ApplicationProperties {
+    public final static String URL_DB = "jdbc:postgresql://localhost:5432/postgres";
+    public final static String User_Name = "postgres";
+    public final static String PASS = "ghazal";
+}
